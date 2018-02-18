a195034f055f0836633866bb0e687f9d8f7d521a
Daniel <daniel.jungen@t-online.de>
2018-02-18 15:51:32 +0100

    updates

--- ui.R
+++ ui.R
@@ -13,7 +13,7 @@ library(shiny)
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Simulation of n dice rolls"),
+  titlePanel("Simulation of throwing a die with s sides n times"),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
